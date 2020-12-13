faa8cdcf1ab0b0b85d8e69c919e02291127b0a3d
sonmh <mhongson95@gmail.com>
2020-12-13 15:05:21 +0700

    update

--- src/todo_list.tsx
+++ src/todo_list.tsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { View, StyleSheet, FlatList } from 'react-native';
+import { View, StyleSheet, FlatList, Text } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { useSelector } from 'react-redux';
 
@@ -7,6 +7,7 @@ import AddButton from './element/add_button';
 import TodoItem from './element/todo_item';
 import {AppState} from './redux/reducer';
 import SlideFromBottomItem from './element/animations/from_bottom';
+import platfrom from './utils/platfrom';
 
 type RootStackParamList = {
     TodoList: undefined;
@@ -62,6 +63,24 @@ const TodoList: React.FC<Props> = (props) => {
                     );
                 }}
             />
+
+            <View style={{paddingVertical: 20}}>
+                <Text style={styles.titleNote}>
+                    {'*Chú thích:'}
+                </Text>
+                <View style={{flexDirection: 'row', alignItems: 'flex-start', marginTop: 8}}>
+                    <View style={{width: 15, height: 15, backgroundColor: platfrom.activeColor, borderRadius: 7.5}} />
+                    <Text style={{fontSize: 13, paddingLeft: 8}}>{'Đã hoàn thành'}</Text>
+                </View>
+                <View style={{flexDirection: 'row', alignItems: 'flex-start', marginTop: 8}}>
+                    <View style={{width: 15, height: 15, backgroundColor: platfrom.alertColor, borderRadius: 7.5}} />
+                    <Text style={{fontSize: 13, paddingLeft: 8}}>{'Chưa hoàn thành'}</Text>
+                </View>
+                <View style={{flexDirection: 'row', alignItems: 'flex-start', marginTop: 8}}>
+                    <View style={{width: 15, height: 15, backgroundColor: '#DDA0DD', borderRadius: 7.5}} />
+                    <Text style={{fontSize: 13, paddingLeft: 8}}>{'Đang thực hiện'}</Text>
+                </View>
+            </View>
         </View>
     );
 };
@@ -74,5 +93,10 @@ const styles = StyleSheet.create({
         paddingHorizontal: 16,
         paddingVertical: 30,
         backgroundColor: '#fff',
+    },
+    titleNote: {
+        fontSize: 14,
+        fontWeight: 'bold',
+        color: '#000'
     },
 });
